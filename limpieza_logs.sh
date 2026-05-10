2cf0eed8b7fc9dedaaa48f7d8a339d59039d9cf2
EC2 Default User <ec2-user@ip-172-31-2-138.ec2.internal>
2026-05-10 18:06:33 +0000

    Cambio de prueba para disparar pipeline

---
+++ limpieza_logs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Eliminar archivos .log
+find ~/environment/proyecto/devops-soluciones-futuro -name "*.log" -type f -delete
+echo "Logs eliminados"
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Instalar boto3
+pip3 install boto3 --user
+echo "boto3 instalado correctamente"
